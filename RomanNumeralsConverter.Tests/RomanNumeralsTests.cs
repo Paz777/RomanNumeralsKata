45dd5fca5ac612146a5dd6752231f7bc8624708c
pazsonagara@gmail.com <pazsonagara@Pazs-MacBook-Air.local>
2022-09-09 13:32:54 +0100

    Added multiple test cases for valid roman numerals all passing

--- RomanNumeralsConverter.Tests/RomanNumeralsTests.cs
+++ RomanNumeralsConverter.Tests/RomanNumeralsTests.cs
@@ -14,10 +14,40 @@ namespace RomanNumeralsConverter.Tests
             romanNumerals = new RomanNumerals();
         }
 
-        [Test]
-        public void Given_A_Roman_Numerals_ConvertToNumber_Should_Return_A_Valid_Number()
+
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("XLII", 42)]
+        [TestCase("MMXIII", 2013)]
+        [TestCase("MXI", 1011)]
+        [TestCase("MCDXCIX", 1499)]
+        [TestCase("MMXXII", 2022)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("CX", 110)]
+        [TestCase("CCCLXXV", 375)]
+        [TestCase("MD", 1500)]
+        [TestCase("MDLXXV", 1575)]
+        [TestCase("MDCL", 1650)]
+        [TestCase("MDCCXXV", 1725)]
+        [TestCase("MDCCC", 1800)]
+        [TestCase("MDCCCLXXV", 1875)]
+        [TestCase("MCML", 1950)]
+        [TestCase("MMXXV", 2025)]
+        [TestCase("MMC", 2100)]
+        [TestCase("MMCLXXV", 2175)]
+        [TestCase("MMCCL", 2250)]
+        [TestCase("MMCCCXXV", 2325)]
+        [TestCase("MMCD", 2400)]
+        [TestCase("MMCDLXXV", 2475)]
+        [TestCase("MMDL", 2550)]
+        [TestCase("MMMMMMMM", 8000)]
+        [TestCase("MMMMMMMMIV", 8004)]
+        public void Given_A_Roman_Numerals_ConvertToNumber_Should_Return_A_Valid_Number(string romanNumber, int expectedNumber)
         {
-            romanNumerals.ConvertToNumber("I").Should().Be(1);
+            romanNumerals.ConvertToNumber(romanNumber).Should().Be(expectedNumber);
         }
     }
 }
--- RomanNumeralsConverter/RomanNumerals.cs
+++ RomanNumeralsConverter/RomanNumerals.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace RomanNumeralsConverter
 {
     public class RomanNumerals
     {
+        private Dictionary<char, int> romanNumbersMap = new Dictionary<char, int>()
+        {
+            {'I', 1 },
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
         public RomanNumerals()
         {
         }
 
         public int ConvertToNumber(string romanNumeral)
         {
-            return 1;
+            int result = 0;
+            for (int index = romanNumeral.Length - 1, last = 0; index >= 0; index--)
+            {
+                int current = romanNumbersMap[romanNumeral.ToCharArray()[index]];
+                result += (current < last ? -current : current);
+                last = current;
+            }
+            return result;
         }
     }
 }
